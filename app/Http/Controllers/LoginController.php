38050c66cfe4ade4421dafe9c9f6c6067c24e039
Maman` <75215687+JfeStudio@users.noreply.github.com>
2023-01-02 01:04:31 +0700

    kita akan bermain dengan attempt

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -39,21 +39,21 @@ class LoginController extends Controller
     public function store(Request $request)
     {
         // buat validasi untuk login sesuai form yang tersedia
-        $request->validate([
+        $creadentials = $request->validate([
             'email' => 'required|min:3|email',
             'password' => 'required|min:8',
         ]);
-        // lalu kita cek user email yang login
-        $user = User::whereEmail($request->email)->first();
-        // kita kasih kondisi, klw emailnya bener kita masuk step selanjutnya
-        if ($user) {
-            // step selanjutnya check password yang di ketikkan di form input apakah sama dengan password user di databasenya
-            if (Hash::check($request->password, $user->password)) {
-                // klw sama kita ksih akses
-                Auth::login($user);
-                // lalu mau di redirect kemana klw udah benar + session flash/with
-                return redirect('/dashboard')->with('success', 'Selamat Anda Berhasil Login');
-            }
+        // step awall bisa pake ini, lalu masukkan variablenya ke dalam pengecekan di attempt
+        // $creadentials = [
+        //     'email' => $request->email,
+        //     'password' => $request->password,
+        // ];
+        // lalu pake only request juga bisa :
+        // $creadentials = $request->only('email', 'password');
+        // terakhir pake langsung di validationnya juga bisa
+        // kita gunakan attempt untuk pengecekan
+        if (Auth::attempt($creadentials)) {
+            return redirect('/dashboard')->with('success', 'Selamat Anda Berhasil Login');
         }
         // tapi jika error, kita buat throw messagenya
         throw ValidationException::withMessages([
