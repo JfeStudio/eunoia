e9955a5ad9b0251ab7a5556b459268fa4848b323
Maman` <75215687+JfeStudio@users.noreply.github.com>
2023-01-05 07:36:39 +0700

    seeder tidak buruk juga

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Task;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,10 +17,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // eksternal seeder
+        // $this->call([
+        // ExampleSeeder::class,
+        // ]);
+
+        $tasks = [
+                    [
+                    'list' => fake()->name(),
+                    'mark' => rand(0, 1),
+                    ],
+                    [
+                    'list' => fake()->name(),
+                    'mark' => rand(0, 1),
+                    ],
+                    [
+                    'list' => 'hari ini aku hidup sehat',
+                    'mark' => rand(0, 1),
+                    ]
+                 ];
+                 foreach ($tasks as $task) {
+                     Task::create($task);
+                 }
     }
 }
